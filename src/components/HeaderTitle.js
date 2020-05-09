af8d61eb6da0f67881aef34e3c9726451c1ee942
capricorncd <1355952333@qq.com>
2020-05-09 10:37:10 +0900

    fix bug that text too long out of screen of android navigator

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -4,24 +4,32 @@
  * Date: 2020-05-08 23:56
  */
 import React, { Component } from 'react'
-import { View, Text, Platform } from 'react-native'
+import { View, Text, Platform, Dimensions } from 'react-native'
 import { COLORS_WHITE } from '../configs'
 
 // tab button width
 export const TAB_BUTTON_WIDTH = 44
+const windowWidth = Dimensions.get('window').width
+
+const defaultStyles = {
+    height: '100%',
+    justifyContent: 'center',
+    marginLeft: Platform.OS === 'ios' ? TAB_BUTTON_WIDTH : -20,
+    marginRight: TAB_BUTTON_WIDTH
+}
 
 export default class HeaderTitle extends Component {
     render () {
         const { title, wrapperStyles, textStyles } = this.props
-        return <View
-            style={{
-                height: '100%',
-                justifyContent: 'center',
-                marginLeft: Platform.OS === 'ios' ? TAB_BUTTON_WIDTH : 0,
-                marginRight: TAB_BUTTON_WIDTH,
-                ...wrapperStyles
-            }}
-        >
+        const styles = {
+            ...defaultStyles,
+            ...wrapperStyles
+        }
+        // fix bug that text too long out of screen of android navigator
+        if (Platform.OS === 'android') {
+            styles.width = windowWidth - TAB_BUTTON_WIDTH * 2
+        }
+        return <View style={styles}>
             <Text
                 numberOfLines={1}
                 style={{
